9df304e0e57f43669c22db13aa9b4c7b6e57f374
rakesh5208 <rakesh5208@gmail.com>
2023-09-10 21:33:15 +0530

    should show lint error

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,7 @@
 import Route from "@ember/routing/route";
-
+const LINT_ERROR = "";
 export default class ApplicationRoute extends Route {
+    beforeModel() {
+        console.log("Should show the lint error");
+    }
 }
