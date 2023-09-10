97d800553308c75cbb2ef48ac1b59c0a0c1b535f
rakesh5208 <rakesh5208@gmail.com>
2023-09-10 21:09:41 +0530

    new PR to for the git action

--- app/components/module-dashboard/component.js
+++ app/components/module-dashboard/component.js
@@ -1,4 +1,19 @@
 import Component from '@glimmer/component';
 
 export default class ModuleDashboardComponent extends Component {
+    get defaultWidgets() {
+        return [
+            {id: '1_default', name: '1_default', label: 'Default 1', default: true},
+            {id: '2_default', name: '2_default', label: 'Default 2', default: true},
+            {id: '3_default', name: '3_default', label: 'Default 3', default: true},
+            {id: '4_default', name: '4_default', label: 'Default 4', default: true},
+            {id: '5_default', name: '5_default', label: 'Default 5', default: true},
+            {id: '6_default', name: '6_default', label: 'Default 6', default: true},
+            {id: '7_default', name: '7_default', label: 'Default 7', default: true}
+        ]
+    }
+
+    get widgets() {
+        return [...this.defaultWidgets];
+    }
 }
